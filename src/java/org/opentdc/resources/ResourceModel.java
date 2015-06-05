a1721214dc9db665df391248e81e013a6acf47dd
Bruno Kaiser <bruno@bkaiser.ch>
2015-06-05 19:35:03 +0200

    added ResourceComparator (based on id)

--- src/java/org/opentdc/resources/ResourceModel.java
+++ src/java/org/opentdc/resources/ResourceModel.java
@@ -23,6 +23,7 @@
  */
 package org.opentdc.resources;
 
+import java.util.Comparator;
 import java.util.Date;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -32,7 +33,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement
 public class ResourceModel {
-	private String id;
+	private String id;			// sortable
 	private String name;
 	private String firstName;
 	private String lastName;
@@ -130,4 +131,26 @@ public class ResourceModel {
 	public void setModifiedBy(String modifiedBy) {
 		this.modifiedBy = modifiedBy;
 	}
+	
+	/******************************* Comparator *****************************/
+	public static Comparator<ResourceModel> ResourceComparator = new Comparator<ResourceModel>() {
+
+		public int compare(ResourceModel obj1, ResourceModel obj2) {
+			if (obj1.getId() == null) {
+				return -1;
+			}
+			if (obj2.getId() == null) {
+				return 1;
+			}
+
+			String _attr1 = obj1.getId();
+			String _attr2 = obj2.getId();
+
+			// ascending order
+			return _attr1.compareTo(_attr2);
+
+			// descending order
+			// return _attr2.compareTo(_attr1);
+		}
+	};
 }
